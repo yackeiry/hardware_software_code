f43f0f7be27a2342d684009c06a581bddce82b2e
yackeiry <Yackeiry@Yackeirys-MacBook-Air.local>
2025-01-27 17:00:35 -0500

    project02

---
+++ YD_project_02.py
@@ -0,0 +1,26 @@
+def main(): 
+    print("welcome to my conversation program!")
+    print("I will brelfy ask you questions intill u type exit.")
+    name = input("What is your name?")
+    if name == "Yackeiry De La Cruz":
+      name = input("Do you like programming, Yackeiry De La Cruz?")
+    if name == "Yes i like programming":
+      print("Wow im so proud that u like programming!")
+    name = input("What is your favorite show, Yackeiry De La Cruz?")
+    if name == "My fav show is Attack on Titan":
+      print("Great Show Yackeiry!")
+    name = input("What is your favorite sport, Yackeiry De La Cruz?")
+    if name == "My fav sport is soccer":
+      print("What a Cool Sport!")
+    name = input("What is your fav food, Yackeiry De La Cruz?")
+    if name == "My fav food is macaroni and cheese":
+      print("Yummy food Yackeiry!")
+    name = input("What Do u like programming, Yackeiry De La Cruz?")
+    if name == "Yes i like programming":
+      print("Wow im so proud that u like programming!")
+    name = input("Do u have pets, Yackeiry De La Cruz?""Exit")
+    print("Thanks for chatting with me, Yackeiry De La Cruz!")
+    print("You answered 4 questions") 
+
+if __name__ == "__main__":
+  main()
