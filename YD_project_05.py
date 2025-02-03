8270f5c85708e78c9fb8c60206458b28a59613af
yackeiry <yackeiryd@gmail.com>
2025-02-03 16:27:32 -0500

    Add files via upload

---
+++ YD_project_05.py
@@ -0,0 +1,33 @@
+def main():
+ print("Hello! Im Yackeiry Program!")
+print("Welcome to our program im so exicted to see u!")
+print("Lets learn and program some numbers!")
+print("This program adds two numbers")
+num1 = input("Enter first number:")
+print("13 is a good number")
+num2 = input("Enter second number:")
+print("14 is a good number")
+print("Let's do some adding!")
+total = num1 + num2
+print("13 + 14 = 27")
+print("Type 'yes' to end program:")
+num1 = input("Enter first number:")
+print("Invaild Number, Try again!!!")
+num1 = input("Enter first number:")
+print("Invaild Number, Try again!!!")
+num1 = input("Enter first number:")
+print("80 is a good number")
+num2 = input("Enter second number:")
+print("Invaild Number, Try again!!!")
+num2 = input("Enter second number:")
+print("90 is a good number")
+print("Lets do some adding!")
+total = num1 + num2
+print("89 + 90 = 179")
+print("Type 'yes' to end program: yes")
+print("Good bye!!!")
+print("Come back when you have more numbers")
+
+
+if __name__ == "__main__":
+ main()
