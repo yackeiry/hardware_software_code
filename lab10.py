e02e9246d21d6f63a0b69e72ec38c73554407583
yackeiry <yackeiryd@gmail.com>
2025-02-04 14:07:14 -0500

    Add files via upload

---
+++ lab10.py
@@ -0,0 +1,13 @@
+def main():
+  print("This program adds two numbers. ")
+
+  num1 = input("Enter first number: ")
+  num2 = input("Enter second number: ")
+  
+  total = num1 + num2
+  print("{} + {} = {}".format(num1, num2, total))
+
+
+
+if __name__ == "__main__":
+  main()
