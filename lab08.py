474f8ab8294f930c4ad56cbf457718acca8ab01c
yackeiry <yackeiryd@gmail.com>
2025-02-05 13:12:52 -0500

    Add files via upload

---
+++ lab08.py
@@ -0,0 +1,12 @@
+def print_lyrics():
+  print ("I'm a programmer, and I'm okay.")
+  print ("I code all night, and I code all day.")
+def repeat_lyrics():
+ print_lyrics()
+ print_lyrics()
+def main():
+  repeat_lyrics()
+
+
+if __name__ == "__main__":
+   main()
