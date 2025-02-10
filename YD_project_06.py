ffca63e6950a52952cb5c14822f04e4110203214
yackeiry <yackeiryd@gmail.com>
2025-02-10 14:37:22 -0500

    Add files via upload

---
+++ YD_project_06.py
@@ -0,0 +1,36 @@
+def greetings():
+  print("Welcome back to our conversation!")
+  print("Im so glad your back Yackeiry!")
+  print("Lets do some number conversion")
+  print("Choose the following number conversion")
+  choose = input("1 binary to decimal")
+  choose = input("2 decimal to binary")
+  choose = input("3 binary to hexadecimal")
+  choose = input("4 decimal to hexadecimal")
+  choose = input("5 hexadecimal to decimal")
+  choose = input("6 hexadecimal to binary")
+  choose = input("7 octal to decimal")
+  choose = input("8 decimal to octal")
+  choose = input("9 octal to hexadecimal")
+  print("Selection: 1")
+  if choose == "binary to decimal":
+  print("Converting Binary to Decimal")
+  print("'0','1' are valid numbers")
+  print("Enter a valid Binary number:Y8")
+  print("Invail Number try again!!")
+  print("Enter a vail Binary number:1011")
+  print("1 * 2 power 3) + (0 * 2 power 2) + (1 * 2 power 1) + (1 * 2 power 0) 8 + 0 + 2 + 1")
+  print("Binary:1011")
+  print("Decimal:11")
+  print("Hit 'Enter' to Continue")
+  print("Type yes to end program")
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+ 	greetings()
